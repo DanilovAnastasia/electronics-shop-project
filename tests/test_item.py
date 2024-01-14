3197226d7f161612fbbff21a5aed42009ebc7ab4
DanilovAnastasia <grey-mice@mail.ru>
2024-01-15 00:45:16 +0300

    Написали класс товаров, затестили, проверили, написали дисконт цены и общую цену всех товаров

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_init():
+    return Item("iphone14_plus", 78000, 10)
+
+
+def test_first(item_init):
+    assert 78000 * 10 == item_init.calculate_total_price()
+
+    Item.pay_rate = 0.8
+    item_init.apply_discount()
+    assert item_init.price == 78000 * 0.8
