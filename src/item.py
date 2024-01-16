4bf75ff01a02f07903b5d9ef494b66e4086889c9
DanilovAnastasia <grey-mice@mail.ru>
2024-01-17 00:37:36 +0300

    add 2 homework

--- src/item.py
+++ src/item.py
@@ -1,3 +1,10 @@
+from csv import DictReader
+
+
+class InstantiateCSVError(Exception):
+    pass
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -17,6 +24,41 @@ class Item:
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, str_name):
+        if len(str_name) <= 10:
+            self.__name = str_name
+        else:
+            self.__name = str_name[0:10]
+            raise Exception("длина наименования товара больше 10 символов")
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path):
+        Item.all = []
+        try:
+            with open('../'+file_path, 'r', encoding='cp1251') as csv_file:
+                items = DictReader(csv_file, delimiter=',')
+                for row in items:
+                    if len(items.fieldnames) == 3 and len(row) == 3:
+                        cls(row['name'], row['price'], row['quantity'])
+                    else:
+                        raise InstantiateCSVError('Формат файла не соответсвует')
+        except KeyError:
+            raise InstantiateCSVError("Файл поврежден")
+        except FileNotFoundError:
+            raise FileNotFoundError("Отсутствует файл items.csv")
+
+    @staticmethod
+    def string_to_number(str):
+        try:
+            return int(float(str))
+        except ValueError:
+            return "Невозможно преобразовать строку в натуральное число"
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -14,3 +14,19 @@ def test_first(item_init):
     Item.pay_rate = 0.8
     item_init.apply_discount()
     assert item_init.price == 78000 * 0.8
+
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+    item.name = 'СуперСмартфон'
+
+    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
